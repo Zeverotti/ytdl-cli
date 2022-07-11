eb4835346fe0d54285c06618b19bfe1691b03191
JMax45 <maxmailru.45@gmail.com>
2022-07-11 11:00:21 +0000

    test: check video duration after download
    Added a video duration check during testing, it has a configurable
    tolerance because the results from ytdl-core are sometimes rounded, the
    current tolerance is 5 seconds.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,13 @@
 import path from 'path';
 import { exec, ExecException } from 'child_process';
 import fs from 'fs';
+import pathToFfmpeg from 'ffmpeg-static';
+import { hmsToNumeric } from '../src/utils/timeConversion';
+import ytdl from 'ytdl-core';
 
 const VIDEO_URL = 'https://youtu.be/d1VR2MMUVO0';
 const OUTPUT_FOLDER = './testfiles';
+const LENGTH_TOLERANCE = 5;
 
 interface res {
   code: number;
@@ -32,6 +36,30 @@ function cli(args: string[], cwd: string, errorLogging = false): Promise<res> {
   });
 }
 
+const getDuration = (path: string): Promise<number> => {
+  return new Promise((resolve, reject) => {
+    exec(`${pathToFfmpeg} -i '${path}'`, (error, stdout, stderr) => {
+      const str = stdout || stderr;
+      const pos = str.search('Duration:');
+      let end = pos;
+      let loop = true;
+      let i = pos;
+      while (loop) {
+        if (str[i] === ',') loop = false;
+        end = i;
+        i += 1;
+      }
+      let extractedString = str.substring(pos, end);
+      extractedString = extractedString.replace('Duration: ', '').trim();
+      resolve(hmsToNumeric(extractedString).seconds);
+    });
+  });
+};
+
+const isBetween = (x: number, min: number, max: number) => {
+  return x >= min && x <= max;
+};
+
 beforeAll(() => {
   return fs.mkdirSync(OUTPUT_FOLDER);
 });
@@ -39,7 +67,18 @@ beforeAll(() => {
 describe('Test cli', () => {
   test('Test mp4 download', async () => {
     let result = await cli(['-l', VIDEO_URL, '-o', OUTPUT_FOLDER], '.', true);
+    const { videoDetails } = await ytdl.getBasicInfo(VIDEO_URL);
+    const duration = await getDuration(
+      `${OUTPUT_FOLDER}/${videoDetails.title}.mp4`
+    );
+    const ytDuration = parseInt(videoDetails.lengthSeconds);
+    const durationRes = isBetween(
+      duration,
+      ytDuration - LENGTH_TOLERANCE,
+      ytDuration + LENGTH_TOLERANCE
+    );
     expect(result.code).toBe(0);
+    expect(durationRes).toBe(true);
   });
   test('Test mp3 download', async () => {
     let result = await cli(
@@ -47,7 +86,18 @@ describe('Test cli', () => {
       '.',
       true
     );
+    const { videoDetails } = await ytdl.getBasicInfo(VIDEO_URL);
+    const duration = await getDuration(
+      `${OUTPUT_FOLDER}/${videoDetails.title}.mp3`
+    );
+    const ytDuration = parseInt(videoDetails.lengthSeconds);
+    const durationRes = isBetween(
+      duration,
+      ytDuration - LENGTH_TOLERANCE,
+      ytDuration + LENGTH_TOLERANCE
+    );
     expect(result.code).toBe(0);
+    expect(durationRes).toBe(true);
   });
   test('Test thumbnail download', async () => {
     let result = await cli(
